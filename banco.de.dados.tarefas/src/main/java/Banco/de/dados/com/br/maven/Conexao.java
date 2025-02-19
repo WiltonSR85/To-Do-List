b97f57e43dc86d129eb0ad1a77b7a13d9c810d3e
Wilton Silva <wiltonsilvarodrigues4321@gmail.com>
2025-02-18 21:52:34 -0300

    Update Conexao.java

--- banco.de.dados.tarefas/src/main/java/Banco/de/dados/com/br/maven/Conexao.java
+++ banco.de.dados.tarefas/src/main/java/Banco/de/dados/com/br/maven/Conexao.java
@@ -1,18 +1,2 @@
-package Banco.de.dados.com.br.maven;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class Conexao {
-
-    
-    private static final String URL = "jdbc:mysql://localhost:3306/dbtarefa";
-    private static final String USUARIO = "LPOO";
-    private static final String SENHA = "south";
-
-    public static Connection obterConexao() throws SQLException{
-        return DriverManager.getConnection(URL, USUARIO, SENHA);
-    }
-}
     
